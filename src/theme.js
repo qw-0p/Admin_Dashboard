1f05db8545db8b80c2d99d894e765d22d12c5d5a
Aleksandr Sarkisov <oleksandr.sarkisov@onereach.com>
2024-07-24 13:45:25 +0300

    feat: add topbar

--- src/theme.js
+++ src/theme.js
@@ -1,8 +1,9 @@
-import { createTheme } from "@mui/material/styles";
 import { createContext, useState, useMemo } from "react";
+import { createTheme } from "@mui/material/styles";
 
+// color design tokens export
 export const tokens = (mode) => ({
-	...(mode === 'dark'
+	...(mode === "dark"
 		? {
 			grey: {
 				100: "#e0e0e0",
@@ -13,18 +14,18 @@ export const tokens = (mode) => ({
 				600: "#525252",
 				700: "#3d3d3d",
 				800: "#292929",
-				900: "#141414"
+				900: "#141414",
 			},
 			primary: {
 				100: "#d0d1d5",
 				200: "#a1a4ab",
 				300: "#727681",
-				400: "#434957",
+				400: "#1F2A40",
 				500: "#141b2d",
 				600: "#101624",
 				700: "#0c101b",
 				800: "#080b12",
-				900: "#040509"
+				900: "#040509",
 			},
 			greenAccent: {
 				100: "#dbf5ee",
@@ -35,7 +36,7 @@ export const tokens = (mode) => ({
 				600: "#3da58a",
 				700: "#2e7c67",
 				800: "#1e5245",
-				900: "#0f2922"
+				900: "#0f2922",
 			},
 			redAccent: {
 				100: "#f8dcdb",
@@ -46,7 +47,7 @@ export const tokens = (mode) => ({
 				600: "#af3f3b",
 				700: "#832f2c",
 				800: "#58201e",
-				900: "#2c100f"
+				900: "#2c100f",
 			},
 			blueAccent: {
 				100: "#e1e2fe",
@@ -57,9 +58,10 @@ export const tokens = (mode) => ({
 				600: "#535ac8",
 				700: "#3e4396",
 				800: "#2a2d64",
-				900: "#151632"
+				900: "#151632",
 			},
-		} : {
+		}
+		: {
 			grey: {
 				100: "#141414",
 				200: "#292929",
@@ -75,9 +77,9 @@ export const tokens = (mode) => ({
 				100: "#040509",
 				200: "#080b12",
 				300: "#0c101b",
-				400: "#f2f0f0",
+				400: "#f2f0f0", // manually changed
 				500: "#141b2d",
-				600: "#434957",
+				600: "#1F2A40",
 				700: "#727681",
 				800: "#a1a4ab",
 				900: "#d0d1d5",
@@ -114,96 +116,99 @@ export const tokens = (mode) => ({
 				700: "#a4a9fc",
 				800: "#c3c6fd",
 				900: "#e1e2fe",
-			}
-		}
-	)
+			},
+		}),
 });
 
+// mui theme settings
 export const themeSettings = (mode) => {
-	const colors = tokens(mode)
-
+	const colors = tokens(mode);
 	return {
 		palette: {
 			mode: mode,
-			...(mode === 'dark'
+			...(mode === "dark"
 				? {
+					// palette values for dark mode
 					primary: {
-						main: colors.primary[500]
+						main: colors.primary[500],
 					},
 					secondary: {
-						main: colors.greenAccent[500]
+						main: colors.greenAccent[500],
 					},
 					neutral: {
 						dark: colors.grey[700],
 						main: colors.grey[500],
-						light: colors.grey[100]
+						light: colors.grey[100],
 					},
 					background: {
-						default: colors.primary[500]
-					}
-				} : {
-					primary: {
-						main: colors.primary[100]
+						default: colors.primary[500],
 					},
-					secondary: {
-						main: colors.greenAccent[500]
-					},
-					neutral: {
-						dark: colors.grey[700],
-						main: colors.grey[500],
-						light: colors.grey[100]
-					},
-					background: {
-						default: "#fcfcfc"
-					}
 				}
-			)
+				: {
+					// palette values for light mode
+					primary: {
+						main: colors.primary[100],
+					},
+					secondary: {
+						main: colors.greenAccent[500],
+					},
+					neutral: {
+						dark: colors.grey[700],
+						main: colors.grey[500],
+						light: colors.grey[100],
+					},
+					background: {
+						default: "#fcfcfc",
+					},
+				}),
 		},
 		typography: {
-			fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+			fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 			fontSize: 12,
 			h1: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 40,
 			},
 			h2: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 32,
 			},
 			h3: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 24,
 			},
 			h4: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 20,
 			},
 			h5: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 16,
 			},
 			h6: {
-				fontFamily: ["Source Sans 3", "sans-serif"].join(","),
+				fontFamily: ["Source Sans Pro", "sans-serif"].join(","),
 				fontSize: 14,
-			}
-		}
-	}
-}
+			},
+		},
+	};
+};
 
-
-export const ColorModeContext = (context) => ({
-	toggleColorMode: () => {}
-})
+// context for color mode
+export const ColorModeContext = createContext({
+	toggleColorMode: () => { },
+});
 
 export const useMode = () => {
-	const [mode, setMode] = useState('dark');
+	const [mode, setMode] = useState("dark");
 
-	const colorMode = useMemo(() => ({
-		toggleColorMode: () => 
-			setMode((prev) => (prev === 'light' ? 'dark' : 'light'))
-	}), [])
+	const colorMode = useMemo(
+		() => ({
+			toggleColorMode: () =>
+				setMode((prev) => (prev === "light" ? "dark" : "light")),
+		}),
+		[]
+	);
 
-	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode])
-
-	return [theme, colorMode]
-}
+	const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+	return [theme, colorMode];
+};
